87678fe55ca5030c48bcd99567076c359c70b197
Edwin van Maastrigt <evanmaastrigt@gmail.com>
2014-10-25 17:58:21 +0200

    Skip test.

--- test/Kwetal/Workdays/Calendar/World/Europe/NetherlandTest.php
+++ test/Kwetal/Workdays/Calendar/World/Europe/NetherlandTest.php
@@ -8,7 +8,7 @@ class NetherlandTest extends PHPUnit_Framework_TestCase
 {
     Public function testNetherland()
     {
-//        $this->markTestSkipped('Does not do anything at this moment');
+        $this->markTestSkipped('Does not do anything at this moment');
 
         $netherland = new Netherland();
 
