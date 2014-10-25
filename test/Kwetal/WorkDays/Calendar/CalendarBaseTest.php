29dad5ed17cef6e1f572f495f7d902e765e3748e
Edwin van Maastrigt <evanmaastrigt@gmail.com>
2014-10-25 17:37:33 +0200

    Unittest for CalendarBase class.

---
+++ test/Kwetal/WorkDays/Calendar/CalendarBaseTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Kwetal\WorkDays\Calendar;
+
+use PHPUnit_Framework_TestCase;
+
+class CalendarBaseTest extends PHPUnit_Framework_TestCase
+{
+    private $object;
+
+    public function setUp()
+    {
+        $this->object = new CalendarBase();
+    }
+
+    public  function testGetWeekendDaysReturnsArrayWithTwoDays()
+    {
+        $value = $this->object->getWeekendDays();
+
+        $this->assertInternalType('array', $value);
+        $this->assertCount(2, $value);
+        $this->assertEquals('Sat', $value[0]);
+        $this->assertEquals('Sun', $value[1]);
+    }
+} 
