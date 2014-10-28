7b7bf98e108ad5a9c19722478fd435b5887da45f
Edwin van Maastrigt <evanmaastrigt@gmail.com>
2014-10-28 13:36:51 +0100

    Added all remaining holidays in Christian calendar.

--- src/Kwetal/Workdays/Traits/ChristianCalendar.php
+++ src/Kwetal/Workdays/Traits/ChristianCalendar.php
@@ -22,6 +22,26 @@ trait ChristianCalendar
     public $hasCleanMonday = false;
 
     /**
+     * @var bool $hasAshWednesday
+     */
+    public $hasAshWednesday = false;
+
+    /**
+     * @var bool $hasHolyThursday
+     */
+    public $hasHolyThursday = false;
+
+    /**
+     * @var bool $hasGoodFriday
+     */
+    public $hasGoodFriday = false;
+
+    /**
+     * @var bool $hasEasterSaturday
+     */
+    public $hasEasterSaturday = false;
+
+    /**
      * @var bool $hasEasterSunday
      */
     public $hasEasterSunday = false;
@@ -32,6 +52,10 @@ trait ChristianCalendar
     public $hasEasterMonday = false;
 
     /**
+     * @var bool $hasAscensionThursday
+     */
+    public $hasAscensionThursday = false;
+    /**
      * @var bool $hasWithSunday
      */
     public $hasWhitSunday = false;
@@ -40,6 +64,46 @@ trait ChristianCalendar
      * @var bool $whitMonday
      */
     public $hasWhitMonday = false;
+
+    /**
+     * @var bool $hasCorpusChristi
+     */
+    public $hasCorpusChristi = false;
+
+    /**
+     * @var bool $hasAssumption
+     */
+    public $hasAssumption = false;
+
+    /**
+     * @var bool $hasAllSaints
+     */
+    public $hasAllSaints = false;
+
+    /**
+     * @var bool $hasAllSouls
+     */
+    public $hasAllSouls = false;
+
+    /**
+     * @var bool $hasImmaculateConception
+     */
+    public $hasImmaculateConception = false;
+
+    /**
+     * @var bool $hasChristmasEve
+     */
+    public $hasChristmasEve = false;
+
+    /**
+     * @var bool $hasChristmasSunday
+     */
+    public $hasChristmasSunday = false;
+
+    /**
+     * @var bool $hasChristmasMonday
+     */
+    public $hasChristmasMonday = false;
 
     /**
      * @param $year
@@ -52,17 +116,36 @@ trait ChristianCalendar
         if ($this->hasCleanMonday) {
             $variableDays[] = $this->getCleanMonday($year);
         }
+        if ($this->hasAshWednesday) {
+            $variableDays[] = $this->getAshWednesday($year);
+        }
+        if ($this->hasHolyThursday) {
+            $variableDays[] = $this->getHolyThursday($year);
+        }
+        if ($this->hasGoodFriday) {
+            $variableDays[] = $this->getGoodFriday($year);
+        }
+        if ($this->hasEasterSaturday) {
+            $variableDays[] = $this->getEasterSaturday($year);
+        }
         if ($this->hasEasterSunday) {
             $variableDays[] = $this->getEasterSunday($year);
         }
         if ($this->hasEasterMonday) {
             $variableDays[] = $this->getEasterMonday($year);
         }
+        if ($this->hasAscensionThursday) {
+            $variableDays[] = $this->getAscensionThursday($year);
+        }
         if ($this->hasWhitSunday) {
             $variableDays[] = $this->getWhitSunday($year);
         }
         if ($this->hasWhitMonday) {
             $variableDays[] = $this->getWhitMonday($year);
+        }
+        if ($this->hasCorpusChristi)
+        {
+            $variableDays[] = $this->getCorpusChristi($year);
         }
 
 
@@ -78,6 +161,27 @@ trait ChristianCalendar
         }
         if ($this->hasAnnunciation){
             $fixedDays[] = $this->getAnnunciation($year);
+        }
+        if ($this->hasAssumption) {
+            $fixedDays[] = $this->getAssumption($year);
+        }
+        if ($this->hasAllSaints) {
+            $fixedDays[] = $this->getAllSaints($year);
+        }
+        if ($this->hasAllSouls) {
+            $fixedDays[] = $this->getAllSouls($year);
+        }
+        if ($this->hasImmaculateConception) {
+            $fixedDays[] = $this->getImmaculateConception($year);
+        }
+        if ($this->hasChristmasEve) {
+            $fixedDays[] = $this->getChristmasEve($year);
+        }
+        if ($this->hasChristmasSunday) {
+            $fixedDays[] = $this->getChristmasSunday($year);
+        }
+        if ($this->hasChristmasMonday) {
+            $fixedDays[] = $this->getChristmasMonday($year);
         }
 
         return $fixedDays;
@@ -100,6 +204,30 @@ trait ChristianCalendar
             ->format('Y-m-d');
     }
 
+    private function getAshWednesday($year) {
+        return DateUtils::getEasterSunday($year)
+            ->sub(new \DateInterval('P46D'))
+            ->format('Y-m-d');
+    }
+
+    private function getHolyThursday($year) {
+        return DateUtils::getEasterSunday($year)
+            ->sub(new \DateInterval('P3D'))
+            ->format('Y-m-d');
+    }
+
+    private function getGoodFriday($year) {
+        return DateUtils::getEasterSunday($year)
+            ->sub(new \DateInterval('P2D'))
+            ->format('Y-m-d');
+    }
+
+    private function getEasterSaturday($year) {
+        return DateUtils::getEasterSunday($year)
+            ->sub(new \DateInterval('P1D'))
+            ->format('Y-m-d');
+    }
+
     private function getEasterSunday($year)
     {
         return DateUtils::getEasterSunday($year)->format('Y-m-d');
@@ -111,6 +239,13 @@ trait ChristianCalendar
         $day->add(new \DateInterval('P1D'));
 
         return $day->format('Y-m-d');
+    }
+
+    private function getAscensionThursday($year)
+    {
+        return DateUtils::getEasterSunday($year)
+            ->add(new \DateInterval('P39D'))
+            ->format('Y-m-d');
     }
 
     private function getWhitSunday($year)
@@ -125,5 +260,47 @@ trait ChristianCalendar
         return DateUtils::getEasterSunday($year)
             ->add(new \DateInterval('P50D'))
             ->format('Y-m-d');
+    }
+
+    private function getCorpusChristi($year)
+    {
+        return DateUtils::getEasterSunday($year)
+            ->add(new \DateInterval('P60D'))
+            ->format('Y-m-d');
+    }
+
+    private function getAssumption($year)
+    {
+        return sprintf('%s-08-15', $year);
+    }
+
+    private function getAllSaints($year)
+    {
+        return sprintf('%s-11-01', $year);
+    }
+
+    private function getAllSouls($year)
+    {
+        return sprintf('%s-11-02', $year);
+    }
+
+    private function getImmaculateConception($year)
+    {
+        return sprintf('%s-12-08', $year);
+    }
+
+    private function getChristmasEve($year)
+    {
+        return sprintf('%s-12-24', $year);
+    }
+
+    private function getChristmasSunday($year)
+    {
+        return sprintf('%s-12-25', $year);
+    }
+
+    private function getChristmasMonday($year)
+    {
+        return sprintf('%s-12-26', $year);
     }
 }
--- test/Kwetal/Workdays/Traits/ChristianCalendarTest.php
+++ test/Kwetal/Workdays/Traits/ChristianCalendarTest.php
@@ -13,11 +13,30 @@ class ChristianCalendarTest extends PHPUnit_Framework_TestCase
         $this->traitObject = $this->getMockForTrait('Kwetal\Workdays\Traits\ChristianCalendar');
     }
 
+    public function hasReturnedValueCorrectStructure($value)
+    {
+        $this->assertInternalType('array', $value);
+        $this->assertCount(1, $value);
+    }
+
     public function testClassStructure()
     {
         $this->assertObjectHasAttribute('hasEpiphany', $this->traitObject);
+        $this->assertObjectHasAttribute('hasAnnunciation', $this->traitObject);
+        $this->assertObjectHasAttribute('hasAllSaints', $this->traitObject);
+        $this->assertObjectHasAttribute('hasImmaculateConception', $this->traitObject);
+        $this->assertObjectHasAttribute('hasChristmasEve', $this->traitObject);
+        $this->assertObjectHasAttribute('hasChristmasSunday', $this->traitObject);
+        $this->assertObjectHasAttribute('hasChristmasMonday', $this->traitObject);
+        $this->assertObjectHasAttribute('hasCleanMonday', $this->traitObject);
+        $this->assertObjectHasAttribute('hasAshWednesday', $this->traitObject);
+        $this->assertObjectHasAttribute('hasHolyThursday', $this->traitObject);
+        $this->assertObjectHasAttribute('hasGoodFriday', $this->traitObject);
+        $this->assertObjectHasAttribute('hasEasterSaturday', $this->traitObject);
         $this->assertObjectHasAttribute('hasEasterSunday', $this->traitObject);
         $this->assertObjectHasAttribute('hasEasterMonday', $this->traitObject);
+        $this->assertObjectHasAttribute('hasWhitSunday', $this->traitObject);
+        $this->assertObjectHasAttribute('hasWhitMonday', $this->traitObject);
     }
 
     public function testGetFixedHolidaysChristianReturnsEmptyArray()
@@ -33,8 +52,7 @@ class ChristianCalendarTest extends PHPUnit_Framework_TestCase
         $this->traitObject->hasEpiphany = true;
         $value = $this->traitObject->getFixedHolidaysChristian(2014);
 
-        $this->assertInternalType('array', $value);
-        $this->assertCount(1, $value);
+        $this->hasReturnedValueCorrectStructure($value);
         $this->assertEquals('2014-01-06', $value[0]);
     }
 
@@ -43,9 +61,71 @@ class ChristianCalendarTest extends PHPUnit_Framework_TestCase
         $this->traitObject->hasAnnunciation = true;
         $value = $this->traitObject->getFixedHolidaysChristian(2014);
 
-        $this->assertInternalType('array', $value);
-        $this->assertCount(1, $value);
+        $this->hasReturnedValueCorrectStructure($value);
         $this->assertEquals('2014-03-25', $value[0]);
+    }
+
+    public function testGetFixedHolidaysChristianReturnsAssumptionDay()
+    {
+        $this->traitObject->hasAssumption = true;
+        $value = $this->traitObject->getFixedHolidaysChristian(2014);
+
+        $this->hasReturnedValueCorrectStructure($value);
+        $this->assertEquals('2014-08-15', $value[0]);
+    }
+
+    public function testGetFixedHolidaysChristianReturnsAllSaintsDay()
+    {
+        $this->traitObject->hasAllSaints = true;
+        $value = $this->traitObject->getFixedHolidaysChristian(2014);
+
+        $this->hasReturnedValueCorrectStructure($value);
+        $this->assertEquals('2014-11-01', $value[0]);
+    }
+
+    public function testGetFixedHolidaysChristianReturnsAllSoulsDay()
+    {
+        $this->traitObject->hasAllSouls = true;
+        $value = $this->traitObject->getFixedHolidaysChristian(2014);
+
+        $this->hasReturnedValueCorrectStructure($value);
+        $this->assertEquals('2014-11-02', $value[0]);
+    }
+
+    public function testGetFixedHolidaysChristianReturnsImmaculateConception()
+    {
+        $this->traitObject->hasImmaculateConception = true;
+        $value = $this->traitObject->getFixedHolidaysChristian(2014);
+
+        $this->hasReturnedValueCorrectStructure($value);
+        $this->assertEquals('2014-12-08', $value[0]);
+    }
+
+    public function testGetFixedHolidaysChristianReturnsChristmasEve()
+    {
+        $this->traitObject->hasChristmasEve = true;
+        $value = $this->traitObject->getFixedHolidaysChristian(2014);
+
+        $this->hasReturnedValueCorrectStructure($value);
+        $this->assertEquals('2014-12-24', $value[0]);
+    }
+
+    public function testGetFixedHolidaysChristianReturnsChristmasSunday()
+    {
+        $this->traitObject->hasChristmasSunday = true;
+        $value = $this->traitObject->getFixedHolidaysChristian(2014);
+
+        $this->hasReturnedValueCorrectStructure($value);
+        $this->assertEquals('2014-12-25', $value[0]);
+    }
+
+    public function testGetFixedHolidaysChristianReturnsChristmasMonday()
+    {
+        $this->traitObject->hasChristmasMonday = true;
+        $value = $this->traitObject->getFixedHolidaysChristian(2014);
+
+        $this->hasReturnedValueCorrectStructure($value);
+        $this->assertEquals('2014-12-26', $value[0]);
     }
 
     public function testGetVariableHolidaysChristianReturnsEmptyArray()
@@ -61,9 +141,44 @@ class ChristianCalendarTest extends PHPUnit_Framework_TestCase
         $this->traitObject->hasCleanMonday = true;
         $value = $this->traitObject->getVariableHolidaysChristian(2014);
 
-        $this->assertInternalType('array', $value);
-        $this->assertCount(1, $value);
+        $this->hasReturnedValueCorrectStructure($value);
         $this->assertEquals('2014-03-03', $value[0]);
+    }
+
+    public function testGetVariableHolidaysChristianReturnsAshWednesday()
+    {
+        $this->traitObject->hasAshWednesday = true;
+        $value = $this->traitObject->getVariableHolidaysChristian(2014);
+
+        $this->hasReturnedValueCorrectStructure($value);
+        $this->assertEquals('2014-03-05', $value[0]);
+    }
+
+    public function testGetVariableHolidaysChristianReturnsHolyThursday()
+    {
+        $this->traitObject->hasHolyThursday = true;
+        $value = $this->traitObject->getVariableHolidaysChristian(2014);
+
+        $this->hasReturnedValueCorrectStructure($value);
+        $this->assertEquals('2014-04-17', $value[0]);
+    }
+
+    public function testGetVariableHolidaysChristianReturnsGoodFriday()
+    {
+        $this->traitObject->hasGoodFriday = true;
+        $value = $this->traitObject->getVariableHolidaysChristian(2014);
+
+        $this->hasReturnedValueCorrectStructure($value);
+        $this->assertEquals('2014-04-18', $value[0]);
+    }
+
+    public function testGetVariableHolidaysChristianReturnsEasterSaturday()
+    {
+        $this->traitObject->hasEasterSaturday = true;
+        $value = $this->traitObject->getVariableHolidaysChristian(2014);
+
+        $this->hasReturnedValueCorrectStructure($value);
+        $this->assertEquals('2014-04-19', $value[0]);
     }
 
     public function testGetVariableHolidaysChristianReturnsEasterSunday()
@@ -71,8 +186,7 @@ class ChristianCalendarTest extends PHPUnit_Framework_TestCase
         $this->traitObject->hasEasterSunday = true;
         $value = $this->traitObject->getVariableHolidaysChristian(2014);
 
-        $this->assertInternalType('array', $value);
-        $this->assertCount(1, $value);
+        $this->hasReturnedValueCorrectStructure($value);
         $this->assertEquals('2014-04-20', $value[0]);
     }
 
@@ -81,9 +195,17 @@ class ChristianCalendarTest extends PHPUnit_Framework_TestCase
         $this->traitObject->hasEasterMonday = true;
         $value = $this->traitObject->getVariableHolidaysChristian(2014);
 
-        $this->assertInternalType('array', $value);
-        $this->assertCount(1, $value);
+        $this->hasReturnedValueCorrectStructure($value);
         $this->assertEquals('2014-04-21', $value[0]);
+    }
+
+    public function testGetVariableHolidaysChristianReturnsAscensionThursday()
+    {
+        $this->traitObject->hasAscensionThursday = true;
+        $value = $this->traitObject->getVariableHolidaysChristian(2014);
+
+        $this->hasReturnedValueCorrectStructure($value);
+        $this->assertEquals('2014-05-29', $value[0]);
     }
 
     public function testGetVariableHolidaysChristianReturnsWhitSunday()
@@ -91,8 +213,7 @@ class ChristianCalendarTest extends PHPUnit_Framework_TestCase
         $this->traitObject->hasWhitSunday = true;
         $value = $this->traitObject->getVariableHolidaysChristian(2014);
 
-        $this->assertInternalType('array', $value);
-        $this->assertCount(1, $value);
+        $this->hasReturnedValueCorrectStructure($value);
         $this->assertEquals('2014-06-08', $value[0]);
     }
 
@@ -101,8 +222,16 @@ class ChristianCalendarTest extends PHPUnit_Framework_TestCase
         $this->traitObject->hasWhitMonday = true;
         $value = $this->traitObject->getVariableHolidaysChristian(2014);
 
-        $this->assertInternalType('array', $value);
-        $this->assertCount(1, $value);
+        $this->hasReturnedValueCorrectStructure($value);
         $this->assertEquals('2014-06-09', $value[0]);
+    }
+
+    public function testGetVariableHolidaysChristianReturnsCorpusChristi()
+    {
+        $this->traitObject->hasCorpusChristi = true;
+        $value = $this->traitObject->getVariableHolidaysChristian(2014);
+
+        $this->hasReturnedValueCorrectStructure($value);
+        $this->assertEquals('2014-06-19', $value[0]);
     }
 } 
