1ae45ff1b75867c031b5eeed6e9a680654fc9e5d
Edwin van Maastrigt <evanmaastrigt@gmail.com>
2014-11-03 20:54:54 +0100

    Use the ReformationDay Trait

--- src/Kwetal/Workdays/Calendar/World/Europe/Germany/Brandenburg.php
+++ src/Kwetal/Workdays/Calendar/World/Europe/Germany/Brandenburg.php
@@ -3,20 +3,18 @@
 namespace Kwetal\Workdays\Calendar\World\Europe\Germany;
 
 use Kwetal\Workdays\Calendar\World\Europe\Germany;
+use Kwetal\Workdays\Traits\ReformationDay;
 
 
 class Brandenburg extends Germany
 {
+    use ReformationDay;
+
     public function getLocalHolidays($year)
     {
         return array_merge(
             parent::getLocalHolidays($year),
             $this->getReformationDay($year)
         );
-    }
-
-    protected function getReformationDay($year)
-    {
-        return [sprintf('%s-10-31', $year)];
     }
 } 
