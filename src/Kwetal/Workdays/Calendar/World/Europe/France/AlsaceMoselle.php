f1c8df805df6aa5b44d16daffaac29e9a0f79f64
Edwin van Maastrigt <evanmaastrigt@gmail.com>
2014-11-08 18:18:32 +0100

    Refactored Alsace Mosselle calendar to use DateTime objects; add doc-comment

--- src/Kwetal/Workdays/Calendar/World/Europe/France/AlsaceMoselle.php
+++ src/Kwetal/Workdays/Calendar/World/Europe/France/AlsaceMoselle.php
@@ -6,10 +6,18 @@ use Kwetal\Workdays\Calendar\World\Europe\France;
 
 class AlsaceMoselle extends France
 {
+    /**
+     * Constructor
+     * 
+     * @param int $year
+     */
     public function __construct($year)
     {
         $this->hasGoodFriday = true;
+        $this->labelGoodFriday = 'Le Vendredi saint)';
+
         $this->hasChristmasMonday = true;
+        $this->labelChristmasMonday = 'Saint-Étienne';
 
         parent::__construct($year);
     }
