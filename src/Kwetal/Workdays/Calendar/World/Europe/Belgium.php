9ce957d602c6ca1fea8dfc09bb3cba88dc94b611
Edwin van Maastrigt <evanmaastrigt@gmail.com>
2014-11-08 18:17:05 +0100

    Refactored Belgium calendar to use DateTime objects; add doc-comment

--- src/Kwetal/Workdays/Calendar/World/Europe/Belgium.php
+++ src/Kwetal/Workdays/Calendar/World/Europe/Belgium.php
@@ -2,6 +2,7 @@
 
 namespace Kwetal\Workdays\Calendar\World\Europe;
 
+use Kwetal\DateUtils\DateTime\DateTime;
 use Kwetal\Workdays\Calendar\CalendarBase;
 use Kwetal\Workdays\Calendar\CalendarInterface;
 use Kwetal\Workdays\Traits\WesternCalendar;
@@ -15,21 +16,49 @@ class Belgium extends CalendarBase implements CalendarInterface
         ChristianCalendar,
         LabourDay;
 
+    /**
+     * Constructor
+     *
+     * @param int $year
+     */
     public function __construct($year)
     {
         $this->hasEasterSunday = true;
+        $this->labelEasterSunday = 'Paaszondag';
+
         $this->hasEasterMonday = true;
+        $this->labelEasterMonday = 'Paasmaandag';
+
         $this->hasAscensionThursday = true;
+        $this->labelAscensionThursday = 'Onze Lieve Heer hemelvaart';
+
         $this->hasWhitSunday = true;
+        $this->labelWhitSunday = 'Pinksterzondag';
+
         $this->hasWhitMonday = true;
+        $this->labelWhitMonday = 'Pinkstermaandag';
+
         $this->hasAssumption = true;
+        $this->labelAssumption = 'Onze Lieve Vrouw hemelvaart';
+
         $this->hasAllSaints = true;
+        $this->labelAllSaints = 'Allerheiligen';
+
         $this->hasChristmasSunday = true;
-        $this->hasChristmasMonday = true;
+        $this->labelChristmasSunday = 'Kerstmis';
+
+        $this->labelNewYearsDay = 'Nieuwjaar';
+
+        $this->labelLabourDay = 'Dag van de arbeid';
 
         $this->loadHolidays($year);
     }
 
+    /**
+     * Load all the holidays for the given year
+     *
+     * @param int $year
+     */
     public function loadHolidays($year)
     {
         if (in_array($year, $this->yearsLoaded)) {
@@ -46,22 +75,44 @@ class Belgium extends CalendarBase implements CalendarInterface
         $this->yearsLoaded[] = $year;
     }
 
+    /**
+     * Returns the local holidays for the given year
+     *
+     * @param int $year
+     * @return array
+     */
     public function getLocalHolidays($year)
     {
         return array_merge(
             $this->getLabourDay($year),
-            $this->getTheOtherLabourDay($year),
+            $this->getNationalDay($year),
             $this->getArmisticeDay($year)
         );
     }
 
-    protected function getTheOtherLabourDay($year)
+    /**
+     * Returns the Belgium National Day
+     *
+     * @param int $year
+     * @return array
+     */
+    protected function getNationalDay($year)
     {
-        return [sprintf('%s-07-21', $year),];
+        $day = new DateTime(sprintf('%s-07-21', $year));
+
+        return [$day->addLabel('Nationale feestdag')];
     }
 
+    /**
+     * Returns Armistice day
+     *
+     * @param int $year
+     * @return array
+     */
     protected function getArmisticeDay($year)
     {
-        return [sprintf('%s-11-11', $year)];
+        $day = new DateTime(sprintf('%s-11-11', $year));
+
+        return [$day->addLabel('Wapenstilstand')];
     }
 }
