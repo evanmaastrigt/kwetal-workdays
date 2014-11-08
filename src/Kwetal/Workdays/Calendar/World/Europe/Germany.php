0b35d1efb95354c53dd2dd24570eadcb9dbf43eb
Edwin van Maastrigt <evanmaastrigt@gmail.com>
2014-11-08 16:03:49 +0100

    Refactored Germany calendar to use DateTime objects; doc-comments.

--- src/Kwetal/Workdays/Calendar/World/Europe/Germany.php
+++ src/Kwetal/Workdays/Calendar/World/Europe/Germany.php
@@ -2,6 +2,7 @@
 
 namespace Kwetal\Workdays\Calendar\World\Europe;
 
+use Kwetal\DateUtils\DateTime\DateTime;
 use Kwetal\Workdays\Calendar\CalendarBase;
 use Kwetal\Workdays\Calendar\CalendarInterface;
 use Kwetal\Workdays\Traits\WesternCalendar;
@@ -15,20 +16,57 @@ class Germany extends CalendarBase implements CalendarInterface
         ChristianCalendar,
         LabourDay;
 
+    /**
+     * Constuctor
+     *
+     * @param int $year
+     */
     public function __construct($year)
     {
         $this->hasEasterSunday = true;
+        $this->labelEasterSunday = 'Ostersontag';
+
         $this->hasEasterMonday = true;
+        $this->labelEasterMonday = 'Ostermontag';
+
         $this->hasAscensionThursday = true;
+        $this->labelAscensionThursday = 'Christi Himmelfahrt';
+
         $this->hasGoodFriday = true;
+        $this->labelGoodFriday = 'Karfreitag';
+
         $this->hasWhitSunday = true;
+        $this->labelWhitSunday = 'Pfingstsontag';
+
         $this->hasWhitMonday = true;
+        $this->labelWhitMonday = 'Pfingstmontag';
+
         $this->hasChristmasSunday = true;
+        $this->labelChristmasSunday = '1. Weinachtstag';
+
         $this->hasChristmasMonday = true;
+        $this->labelChristmasMonday = '2. Weinachtstag';
+
+        $this->labelNewYearsDay = 'Neujahrstag';
+
+        $this->labelEpiphany = 'Heilige Drei Könige';
+
+        $this->labelLabourDay = 'Maifeiertag';
+
+        $this->labelCorpusChristi = 'Fronleichnam';
+
+        $this->labelAssumption = 'Maria Himmelfahrt';
+
+        $this->labelAllSaints = 'Allerheiligen';
 
         $this->loadHolidays($year);
     }
 
+    /**
+     * Loads all the holidays for the given year
+     *
+     * @param int $year
+     */
     public function loadHolidays($year)
     {
         if (in_array($year, $this->yearsLoaded)) {
@@ -45,6 +83,12 @@ class Germany extends CalendarBase implements CalendarInterface
         $this->yearsLoaded[] = $year;
     }
 
+    /**
+     * Returns the local holidays for the given year
+     *
+     * @param int $year
+     * @return array
+     */
     public function getLocalHolidays($year)
     {
         return array_merge(
@@ -53,8 +97,16 @@ class Germany extends CalendarBase implements CalendarInterface
         );
     }
 
+    /**
+     * Returns German Unity Day for the given year
+     * 
+     * @param int $year
+     * @return array
+     */
     protected function getGermanUnityDay($year)
     {
-        return [sprintf('%s-10-03', $year)];
+        $day = new DateTime(sprintf('%s-10-03', $year));
+
+        return [$day->addLabel('Tag der Deutschen Einheit')];
     }
 }
