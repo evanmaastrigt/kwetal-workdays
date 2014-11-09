c4650d32732449d53ea6fff5bfbe557533f6bb2f
Edwin van Maastrigt <evanmaastrigt@gmail.com>
2014-11-09 14:52:04 +0100

    Fixed unittest for France calendar

--- test/Kwetal/Workdays/Calendar/World/Europe/FranceTest.php
+++ test/Kwetal/Workdays/Calendar/World/Europe/FranceTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Kwetal\Workdays\Calendar\World\Europe;
 
+use Kwetal\DateUtils\DateTime\DateTime;
 use PHPUnit_Framework_TestCase;
 
 
@@ -26,7 +27,7 @@ class FranceTest extends PHPUnit_Framework_TestCase
         $this->createCalendar(2014);
         $value = $this->calendar->getHolidays();
 
-        $this->assertCount(13, $value);
+        $this->assertCount(11, $value);
     }
 
     public function testGetHolidaysHasAllHolidays()
@@ -34,34 +35,31 @@ class FranceTest extends PHPUnit_Framework_TestCase
         $this->createCalendar(2014);
         $value = $this->calendar->getHolidays();
 
-        $this->assertContains('2014-01-01', $value);
-        $this->assertContains('2014-04-20', $value);
-        $this->assertContains('2014-04-21', $value);
-        $this->assertContains('2014-05-01', $value);
-        $this->assertContains('2014-05-08', $value);
-        $this->assertContains('2014-05-29', $value);
-        $this->assertContains('2014-06-08', $value);
-        $this->assertContains('2014-06-09', $value);
-        $this->assertContains('2014-07-14', $value);
-        $this->assertContains('2014-08-15', $value);
-        $this->assertContains('2014-11-01', $value);
-        $this->assertContains('2014-11-11', $value);
-        $this->assertContains('2014-12-25', $value);
+        $this->assertTrue(in_array(new DateTime('2014-01-01'), $value));
+        $this->assertTrue(in_array(new DateTime('2014-05-01'), $value));
+        $this->assertTrue(in_array(new DateTime('2014-05-08'), $value));
+        $this->assertTrue(in_array(new DateTime('2014-05-29'), $value));
+        $this->assertTrue(in_array(new DateTime('2014-06-08'), $value));
+        $this->assertTrue(in_array(new DateTime('2014-06-09'), $value));
+        $this->assertTrue(in_array(new DateTime('2014-07-14'), $value));
+        $this->assertTrue(in_array(new DateTime('2014-08-15'), $value));
+        $this->assertTrue(in_array(new DateTime('2014-11-01'), $value));
+        $this->assertTrue(in_array(new DateTime('2014-11-11'), $value));
+        $this->assertTrue(in_array(new DateTime('2014-12-25'), $value));
     }
 
     public function testAddWorkDays()
     {
         $this->createCalendar(2014);
 
-        $day = new \DateTime('2014-07-12');
+        $day = new DateTime('2014-07-12');
         $this->calendar->addWorkdays($day, 1);
 
         $this->assertEquals('2014-07-15', $day->format('Y-m-d'));
 
-        $day = new \DateTime('2014-07-12');
+        $day = new DateTime('2014-07-12');
         $this->calendar->addWorkdays($day, 30);
 
         $this->assertEquals('2014-08-26', $day->format('Y-m-d'));
     }
-
 }
