e584bdde376ec0def0e896295bacbf98770f6f84
Edwin van Maastrigt <evanmaastrigt@gmail.com>
2014-11-09 16:19:49 +0100

    Added unittest for MecklenburgVorpommern calendar

---
+++ test/Kwetal/Workdays/Calendar/World/Europe/Germany/MecklenburgVorpommernTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Kwetal\Workdays\Calendar\World\Europe\Germany;
+
+use Kwetal\DateUtils\DateTime\DateTime;
+use PHPUnit_Framework_TestCase;
+
+class MecklenburgVorpommernTest extends PHPUnit_Framework_TestCase
+{
+    public $calendar;
+
+    public function createCalendar($year)
+    {
+        $this->calendar = new Brandenburg($year);
+    }
+
+    public function testGetAllHolidaysReturnsArray()
+    {
+        $this->createCalendar(2014);
+        $value = $this->calendar->getHolidays();
+
+        $this->assertInternalType('array', $value);
+    }
+
+    public function testGetHolidaysReturnsCorrectCount()
+    {
+        $this->createCalendar(2014);
+        $value = $this->calendar->getHolidays();
+
+        $this->assertCount(12, $value);
+    }
+
+    public function testGetHolidaysHasAllHolidays()
+    {
+        $this->createCalendar(2014);
+        $value = $this->calendar->getHolidays();
+
+        $this->assertTrue(in_array(new DateTime('2014-10-31'), $value));
+    }
+
+    public function testAddWorkDays()
+    {
+        $this->createCalendar(2014);
+
+        $day = new \DateTime('2014-10-03');
+        $this->calendar->addWorkdays($day, 1);
+
+        $this->assertEquals('2014-10-06', $day->format('Y-m-d'));
+
+        $day = new \DateTime('2014-10-03');
+        $this->calendar->addWorkdays($day, 20);
+
+        $this->assertEquals('2014-11-03', $day->format('Y-m-d'));
+    }
+} 
